709c7345cf5b487d590fa91ab4c8a37805e6e5cd
Anders <ande671f@edu.ucl.dk>
2020-11-05 12:24:06 +0100

    lmklm
    klklmkklmklmklmlmklmlmklmklmlkmlkm

--- WebApi/WebApi/Models/News.cs
+++ WebApi/WebApi/Models/News.cs
@@ -11,6 +11,7 @@ namespace WebApi.Models
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public int NewsId { get; set; }
-        public User UserId { get; set; }
+        public string UserId { get; set; }
+        public virtual User user { get; set; }
     }
 }
