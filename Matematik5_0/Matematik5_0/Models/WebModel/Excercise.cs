585ea5cab029d43a2ba2da2a6b0503176fa248fe
Rerup <Jaco222r@edu.ucl.dk>
2020-12-04 10:09:16 +0100

    Hello Models

---
+++ Matematik5_0/Matematik5_0/Models/WebModel/Excercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matematik5_0.Models.WebModel
+{
+    public class Excercise
+    {
+        public int ID { get; set; }
+        public int Score { get; set; }
+        public string Description { get; set; }
+        public int Solution { get; set; }
+        public int ApplicationUserID { get; set; }
+
+        [Required]
+        public int Answer { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        
+
+    }
+}
